c6c681d0d59aeddb5162d5be8ed7f93f0afe85e0
maevanapcontact <maevanap.contact@gmail.com>
2021-01-22 22:07:43 +0100

    [#4] feat: filter by tags logic - creates tags objects

--- src/js/filters.js
+++ src/js/filters.js
@@ -54,18 +54,70 @@ const getAllUstensils = () => {
 };
 
 /**
- * Fill the DOM with all the filter tags
+ * Creates an object with all the tags as key and an empty array as value
  *
- * @return  {void}
+ * @param   {function}  getTagList  The function to get the tag list
+ *
+ * @return  {object}                The object created
  */
-const fillAllFilters = () => {
-  const filterIngredientsElt = document.getElementById("filters-all-1");
-  const filterApplianceElt = document.getElementById("filters-all-2");
-  const filterUstensilsElt = document.getElementById("filters-all-3");
+const createTagObject = (getTagList) => {
+  let tagObj = {};
+  const tagList = getTagList();
+  tagList.forEach((tag) => (tagObj[tag] = []));
 
-  fillSpecificFilter(getAllIngredients(), filterIngredientsElt);
-  fillSpecificFilter(getAllApplicances(), filterApplianceElt);
-  fillSpecificFilter(getAllUstensils(), filterUstensilsElt);
+  return tagObj;
+};
+
+/**
+ * Fills the ingredients tags object values with IDs of recipes
+ *
+ * @return  {object}  The ingredients tags object updated
+ */
+const fillsIngredientsTagsObj = () => {
+  let ingredientsTagObj = createTagObject(getAllIngredients);
+
+  data.recipes.forEach((recipe) => {
+    recipe.ingredients.forEach((elt) => {
+      const objKey = elt.ingredient.toLowerCase();
+      ingredientsTagObj[objKey].push(recipe.id);
+    });
+  });
+
+  return ingredientsTagObj;
+};
+
+/**
+ * Fills the appliances tags object values with IDs of recipes
+ *
+ * @return  {object}  The appliances tags object updated
+ */
+const fillsAppliancesTagsObj = () => {
+  let appliancesTagsObj = createTagObject(getAllApplicances);
+
+  data.recipes.forEach((recipe) => {
+    const objKey = recipe.appliance.toLowerCase();
+    appliancesTagsObj[objKey].push(recipe.id);
+  });
+
+  return appliancesTagsObj;
+};
+
+/**
+ * Fills the ustensils tags object values with IDs of recipes
+ *
+ * @return  {object}  The ustensils tags object updated
+ */
+const fillsUstensilsTagsObj = () => {
+  let ustensilsTagsObj = createTagObject(getAllUstensils);
+
+  data.recipes.forEach((recipe) => {
+    recipe.ustensils.forEach((elt) => {
+      const objKey = elt.toLowerCase();
+      ustensilsTagsObj[objKey].push(recipe.id);
+    });
+  });
+
+  return ustensilsTagsObj;
 };
 
 /**
@@ -85,6 +137,25 @@ const fillSpecificFilter = (filterList, filterElt) => {
   });
 
   filterElt.appendChild(ulElt);
+};
+
+/**
+ * Fill the DOM with all the filter tags
+ *
+ * @return  {void}
+ */
+const fillAllFilters = () => {
+  const filterIngredientsElt = document.getElementById("filters-all-1");
+  const filterApplianceElt = document.getElementById("filters-all-2");
+  const filterUstensilsElt = document.getElementById("filters-all-3");
+
+  fillSpecificFilter(getAllIngredients(), filterIngredientsElt);
+  fillSpecificFilter(getAllApplicances(), filterApplianceElt);
+  fillSpecificFilter(getAllUstensils(), filterUstensilsElt);
+
+  console.log(fillsIngredientsTagsObj());
+  console.log(fillsAppliancesTagsObj());
+  console.log(fillsUstensilsTagsObj());
 };
 
 export { fillAllFilters };
