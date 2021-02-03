2a6936dcc63c1a55c30c2fadf3cf9b297ad6874e
maevanapcontact <maevanap.contact@gmail.com>
2021-02-03 10:58:34 +0100

    [#1] chore: add Jasmine for testing

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,1 @@
 import "../scss/main.scss";
-import { createRecipes } from "./recipes";
-import { fillAllFilters } from "./filters";
-import { manageFilterLists } from "./ui";
-
-createRecipes();
-fillAllFilters();
-manageFilterLists();
